2ee3626afe1a04b345ebaa3eeab5c070203c1ca0
DominikWro <dominik.wronski@gmail.com>
2018-08-06 17:30:12 +0200

    Create endpoint/new
    VIew, Template, urls.py

--- APIable/api/urls.py
+++ APIable/api/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, re_path
 from . import views
 
 urlpatterns = [
-    path('', views.Welcome_View.as_view(), name="welcome"),
-    path('endpoint/list', views.Endpoint_List.as_view(), name='endpoint_list'),
-    path('endpoint/new', views.Create_Endpoint.as_view(), name='endpoint_new'),
+    path('', views.WelcomeView.as_view(), name="welcome"),
+    path('endpoint/list', views.EndpointList.as_view(), name='endpoint_list'),
+    path('endpoint/new', views.CreateEndpoint.as_view(), name='endpoint_new'),
 ]
--- APIable/api/views.py
+++ APIable/api/views.py
@@ -2,25 +2,31 @@ from django.shortcuts import render
 from django.views.generic import View, TemplateView, FormView
 from .models import Endpoint
 from .forms import EndpointForm
+from django.utils.decorators import method_decorator
+from django.views.decorators.csrf import csrf_exempt
 
 
 # Create your views here.
-class Welcome_View(TemplateView):
+class WelcomeView(TemplateView):
     template_name = 'api/index.html'
 
 
-class Endpoint_List(View):
+class EndpointList(View):
     def get(self, request):
         endpoints = Endpoint.objects.all()
         return render(
             request, 'api/endpoint_list.html', {'endpoints': endpoints})
 
 
-class Create_Endpoint(FormView):
+class SuccessView(TemplateView):
+    template_name = 'api/success.html'
+
+
+class CreateEndpoint(FormView):
     template_name = 'api/endpoint_edit.html'
     success_url = '/endpoint/new'
     form_class = EndpointForm
 
     def form_valid(self, form):
         self.object = form.save()
-        return super(Create_Endpoint, self).form_valid(form)
+        return super(CreateEndpoint, self).form_valid(form)
