f4d9f648af4a1eb333fb45357b1e571681d2bc2f
DominikWro <dominik.wronski@gmail.com>
2018-08-06 19:15:48 +0200

    Old APIable folder removed

--- APIable/api/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path, re_path
-from . import views
-
-urlpatterns = [
-    path('', views.WelcomeView.as_view(), name="welcome"),
-    path('endpoint/list', views.EndpointList.as_view(), name='endpoint_list'),
-    path('endpoint/new', views.CreateEndpoint.as_view(), name='endpoint_new'),
-]
